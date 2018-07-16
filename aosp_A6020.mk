7cf559faa7163091224692cafa3961f5b4318c11
harr8242 <mohamedharis97@gmail.com>
2018-07-16 21:08:11 +0530

    A6020: changes for aosp build

--- aosp_A6020.mk
+++ aosp_A6020.mk
@@ -14,9 +14,6 @@
 
 $(call inherit-product, device/lenovo/A6020/full_A6020.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product-if-exists, vendor/lineage/config/common_full_phone.mk)
-
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
@@ -24,7 +21,4 @@ TARGET_BOOTANIMATION_HALF_RES := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := A6020
-PRODUCT_NAME := lineage_A6020
-
-# Unofficial build ID
-TARGET_UNOFFICIAL_BUILD_ID := HYPER
+PRODUCT_NAME := aosp_A6020
--- board/hardware.mk
+++ board/hardware.mk
@@ -1,6 +1,2 @@
 # Qualcomm support
 BOARD_USES_QCOM_HARDWARE := true
-
-# Lineage hardware
-BOARD_HARDWARE_CLASS += \
-    device/lenovo/A6020/lineagehw
